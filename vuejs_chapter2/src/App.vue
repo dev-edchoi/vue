f1a5c8a1c843f0be7226ced9c8ad1c5b8f82b948
Jinyoung <dev.edchoi@gmail.com>
2025-01-08 09:59:45 +0900

    vuejs

---
+++ vuejs_chapter2/src/App.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MyFirstComponent from './components/MyFirstComponent.vue';
+
+export default defineComponent({
+  components : {
+    MyFirstComponent,
+  }
+})
+</script>
+<template>
+  <my-first-component/>
+  <MyFirstComponent />
+</template>
---
+++ vuejs_chapter2/src/components/MyFirstComponent.vue
@@ -0,0 +1,13 @@
+<script lang="ts"> export default {
+    name : 'MyFirstComponent'
+}</script>
+
+<template>
+    <h2 class="heading">I am a Vue MyFirstComponent</h2>
+</template>
+
+<style>
+.heading {
+    font-size: 16px;
+}
+</style>
