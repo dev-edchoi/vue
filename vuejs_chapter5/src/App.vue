b4499e82d4e4c574092d3c7ad7e62f64078763fc
Jinyoung <dev.edchoi@gmail.com>
2025-01-15 17:49:41 +0900

    250115_실전 코드로 배우는 Vue.js-Chapter5

---
+++ vuejs_chapter5/src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MyMessageComponent from './components/MyMessageComponent.vue';
+
+export default defineComponent({
+  components: {
+    MyMessageComponent,
+  }  
+})
+</script>
+
+<template>
+  <MyMessageComponent />
+</template>
+
+
---
+++ vuejs_chapter5/src/components/MyMessageComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <h2 class="heading">{{ message }}</h2>
+        <input type="text" v-model="message" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const message = ref("Hello World")
+const title = "My Message Component"
+</script>
+
+<!-- <script lang="ts">
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        const message = "Welcome to vue3"
+
+        return {
+            message
+        }
+    }
+}
+</script> -->
